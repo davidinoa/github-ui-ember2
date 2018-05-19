c55668ecb2fa94449c2173c679bd4ea6947107c2
David Inoa <davidinoa@gmail.com>
2018-05-19 13:38:47 -0700

    Complete exercise #1

--- app/router.js
+++ app/router.js
@@ -2,11 +2,19 @@ import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
 const Router = EmberRouter.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
+	location: config.locationType
 });
 
 Router.map(function() {
+	this.route('orgs', {});
+	this.route('org', { path: 'org/:id' }, function() {
+		this.route('repos', {});
+		this.route('repo', { path: ':repoid' }, function() {
+			this.route('contributors', {});
+			this.route('issues', {});
+		});
+	});
+	this.route('notfound', { path: '*path' });
 });
 
 export default Router;
