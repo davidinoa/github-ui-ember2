135d92ffbc1026e8ed61ed9d13df5ecface50e95
David Inoa <davidinoa@gmail.com>
2018-05-22 08:45:01 -0700

    Complete exercise #3

--- app/routes/orgs.js
+++ app/routes/orgs.js
@@ -1,6 +1,12 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+	favorites: Ember.inject.service(),
+	actions: {
+		favoriteClicked(org) {
+			this.get('favorites').favoriteItem(org);
+		}
+	},
 	model() {
 		return [
 			{ id: 'emberjs' },
