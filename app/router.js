8f4f7a199ba999bcc927e9fa64232ef0d7a84cb8
David Inoa <davidinoa@gmail.com>
2018-05-26 10:06:11 -0700

    Complete exercise #5

--- app/router.js
+++ app/router.js
@@ -2,23 +2,24 @@ import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
 const Router = EmberRouter.extend({
-	location: config.locationType
+    location: config.locationType
 });
 
 Router.map(function() {
-	// LIST OF ORGS
-	this.route('orgs', {});
-	// INDIVIDUAL ORG
-	this.route('org', { path: 'org/:id' }, function() {
-		// LIST OF REPOS
-		this.route('repos', {});
-		// INDIVIDUAL REPO
-		this.route('repo', { path: ':repoid' }, function() {
-			this.route('contributors', {});
-			this.route('issues', {});
-		});
-	});
-	this.route('notfound', { path: '*path' });
+  // LIST OF ORGS
+  this.route('orgs', {});
+  // INDIVIDUAL ORG
+  this.route('org', { path: 'org/:id' }, function() {
+    // LIST OF REPOS
+    this.route('repos', {});
+    // INDIVIDUAL REPO
+    this.route('repo', { path: ':repoid' }, function() {
+        this.route('contributors', {});
+        this.route('issues', {});
+    });
+    this.route('notfound');
+  });
+  this.route('notfound', { path: '*path' });
 });
 
 export default Router;
--- app/routes/org.js
+++ app/routes/org.js
@@ -1,6 +1,15 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+	actions: {
+		error(jqxhr) {
+			if (jqxhr.status === 404) {
+				this.intermediateTransitionTo('org.notfound');
+			} else {
+				return true; // bubble up
+			}
+		}
+	},
 	model(params) {
 		return $.get(`https://api.github.com/orgs/${params.id}`).then((rawOrg) => {
 			// backing up github's numeric ID
